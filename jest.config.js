0e5382b8d419cec860235eb3af5dae5f9306c42a
Leonard-Prosper <leonardmalcolm00@gmail.com>
2025-09-24 16:37:14 +0100

    feat: configure eslint

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,35 @@
 module.exports = {
     // Use ts-jest preset for TypeScript support
-    preset: 'ts-jest',
+    preset: "ts-jest",
 
     // Set test environment to Node.js
-    testEnvironment: 'node',
+    testEnvironment: "node",
 
     // Setup files to run after Jest environment is set up
-    setupFilesAfterEnv: ['<rootDir>/tests/setup/jest.setup.ts'],
+    setupFilesAfterEnv: ["<rootDir>/tests/setup/jest.setup.ts"],
 
     // Test file patterns
     testMatch: [
-        '<rootDir>/tests/**/*.test.ts',
-        '<rootDir>/tests/**/*.spec.ts',
-        '<rootDir>/tests/**/*.test.js',
-        '<rootDir>/tests/**/*.spec.js'
+        "<rootDir>/tests/**/*.test.ts",
+        "<rootDir>/tests/**/*.spec.ts",
+        "<rootDir>/tests/**/*.test.js",
+        "<rootDir>/tests/**/*.spec.js"
     ],
 
     // Module name mapping for path aliases
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/api/$1',
-        '^@tests/(.*)$': '<rootDir>/tests/$1'
+        "^@/(.*)$": "<rootDir>/api/$1",
+        "^@tests/(.*)$": "<rootDir>/tests/$1"
     },
 
     // Coverage configuration
     collectCoverageFrom: [
-        'api/**/*.ts',
-        '!api/**/*.d.ts',
-        '!api/index.ts',
-        '!api/**/test_routes/**',
-        '!**/node_modules/**',
-        '!**/dist/**'
+        "api/**/*.ts",
+        "!api/**/*.d.ts",
+        "!api/index.ts",
+        "!api/**/test_routes/**",
+        "!**/node_modules/**",
+        "!**/dist/**"
     ],
 
     // Coverage thresholds
@@ -44,23 +44,23 @@ module.exports = {
 
     // Coverage reporters
     coverageReporters: [
-        'text',
-        'text-summary',
-        'html',
-        'lcov'
+        "text",
+        "text-summary",
+        "html",
+        "lcov"
     ],
 
     // Coverage directory
-    coverageDirectory: 'coverage',
+    coverageDirectory: "coverage",
 
     // File extensions to consider
-    moduleFileExtensions: ['ts', 'js', 'json'],
+    moduleFileExtensions: ["ts", "js", "json"],
 
     // Ignore patterns
     testPathIgnorePatterns: [
-        '/node_modules/',
-        '/dist/',
-        '/coverage/'
+        "/node_modules/",
+        "/dist/",
+        "/coverage/"
     ],
 
     // Clear mocks between tests
